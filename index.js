6c6b5e446d8fa37d8dfe9c05fe8d674a29dfdcd2
Kiboom <cheyugy@naver.com>
2015-08-23 00:26:40 +0900

    scroll after focus

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-var curPos = window.scrollY;
 var posting_style = document.querySelector('.posting').style;
 var posting_textbox_style = document.querySelector('.posting-textbox').style;
 var posting_send_style = document.querySelector('.posting-send').style;
@@ -14,11 +13,32 @@ posting_textbox.addEventListener ('focus', function (event) {
     posting_textbox_style.height = '35px';
     posting_send_style.right = '3px';
 
+    var curPos = window.scrollY;
+
+	window.addEventListener ("scroll", function (event) {
+	    
+	    var top = this.scrollY;
+
+	    if (curPos < top) {
+	        posting_style.height = '30px';
+	        posting_style.paddingRight = "15px";
+	        posting_textbox_style.width = '99%';
+	        posting_textbox_style.height = '15px';
+	        posting_send_style.right = '-70px';
+	        curPos = scrollY;
+	    } /* else {
+	        posting_style.paddingRight = '65px';
+	        posting_send_style.right = '3px';
+	        curPos = scrollY;
+	    }*/
+	     
+	}, false);
+
 }, false);
 
 
 
-
+var curPos = window.scrollY;
 
 window.addEventListener ("scroll", function (event) {
     
