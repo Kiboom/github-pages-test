ffa43eb13f2fe1021104f9ef1580b6780e6cfb2e
Kiboom <cheyugy@naver.com>
2015-08-24 12:04:08 +0900

    scroll problem

--- index.js
+++ index.js
@@ -2,13 +2,31 @@ var posting_style = document.querySelector('.posting').style;
 var posting_textbox_style = document.querySelector('.posting-textbox').style;
 var posting_send_style = document.querySelector('.posting-send').style;
 
-
 var posting_textbox = document.querySelector(".posting-textbox");
 
+
+posting_textbox.addEventListener ('focus', function (event) { 
+
+	posting_style.postion = "fixed";
+    posting_style.height = '80px';
+    posting_style.paddingRight = "65px";
+    posting_textbox_style.width = '100%';
+    posting_textbox_style.height = '67px';
+    posting_textbox_style.fontSize = '14pt';    
+    posting_send_style.opacity = 1;
+    posting_send_style.right = "5px";
+
+}, false);
 
 
 
 var curPos = window.scrollY;
+
+var body = document.body,
+    html = document.documentElement;
+
+var height = Math.max( body.scrollHeight, body.offsetHeight, 
+                       html.clientHeight, html.scrollHeight, html.offsetHeight );
 
 window.addEventListener ("scroll", function (event) {
     
@@ -23,11 +41,9 @@ window.addEventListener ("scroll", function (event) {
         posting_send_style.opacity = 0;
         posting_send_style.right = "-100px";
         curPos = scrollY;
-    } /* else {
-        posting_style.paddingRight = '65px';
-        posting_send_style.right = '3px';
+    } else {
         curPos = scrollY;
-    }*/
+    }
      
 }, false);
 
