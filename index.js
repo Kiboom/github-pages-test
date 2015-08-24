26aabcaa21bc5ba48fd417795d8b9bde5d55686b
Kiboom <cheyugy@naver.com>
2015-08-24 12:11:54 +0900

    fix fixed

--- index.js
+++ index.js
@@ -48,8 +48,20 @@ window.addEventListener ("scroll", function (event) {
 }, false);
 
 
+/* we need this only on touch devices */
+if (Modernizr.touch) {
+    /* cache dom references */ 
+    var $body = jQuery('body'); 
 
-
+    /* bind events */
+    $(document)
+    .on('focus', 'input', function() {
+        $body.addClass('fixfixed');
+    })
+    .on('blur', 'input', function() {
+        $body.removeClass('fixfixed');
+    });
+} 
 
 
 
